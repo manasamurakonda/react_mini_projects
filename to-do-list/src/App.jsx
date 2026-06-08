26e8f7e7d31fde975ff131a3cca825b06e32b931
manasamurakonda <murakondamanasa@gmail.com>
2026-06-09 03:00:14 +0530

    done with to-do-list

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css"
 function App(){
   const [tasks,setTasks] = useState([]);
   const [curTask,setCurTask] = useState("");
   const [show,setShow] = useState(false);
+  const [editIndex,setEditIndex] = useState(-1);
+  const [editText,setEditText] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+  const savedTasks = localStorage.getItem("tasks");
+
+  if (savedTasks) {
+    setTasks(JSON.parse(savedTasks));
+  }
+
+  setLoaded(true);
+}, []);
+useEffect(() => {
+  if (!loaded) return;
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}, [tasks, loaded]);
   function displayTextBox(){
     setShow(true);
   }
@@ -11,10 +28,18 @@ function App(){
     const newTasks = [...tasks, curTask];
     setTasks(newTasks);
     setShow(false);
+    setCurTask("");
   }
   function deleteTask(indexToDelete){
     const newTasks = tasks.filter((item, index) => index !== indexToDelete);
     setTasks(newTasks);
+  }
+  function savingEdit(){
+    const newTasks = [...tasks];
+    newTasks[editIndex] = editText;
+    setTasks(newTasks);
+    setEditIndex(-1);
+    setEditText("");
   }
   return(
     <div>
@@ -33,10 +58,35 @@ function App(){
         {
           tasks.map((task, index) => (
           <div className = "displayTasks">
-            <p key={index} className="task">{task}</p>
+            {
+              editIndex === index ? (
+                <input
+                  value={editText}
+                  onChange={(e) => {
+                  setEditText(e.target.value);
+              }}
+              />
+              ) : (
+              <p className="task">{task}</p>
+              )
+            }
+            <div className = "taskButtons">
+            {
+              editIndex === index ? (
+              <button onClick = {savingEdit}>Save</button>
+              ) : (
+              <button onClick={() => {
+                setEditIndex(index);
+                setEditText(task);
+              }}>
+              Edit
+              </button>
+              )
+            }
             <button onClick = {()=>{
               deleteTask(index);
             }}>Delete</button>
+            </div>
           </div>
           ))
         }
